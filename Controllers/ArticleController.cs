5c7af874ee5cadda3f6414d4ef3b50ad119af750
Irkaldas <ka.krawczyk1995@gmail.com>
2022-06-10 19:46:54 +0200

    commit

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArticleController : ControllerBase
+{
+    ArticleController()
+    {
+
+    }
+}
